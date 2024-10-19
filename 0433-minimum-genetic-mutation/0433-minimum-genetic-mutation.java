36916925190f1396a00239c3f62e7cebb717892a
Mohammedshahinsha <119309213+Mohammedshahinsha@users.noreply.github.com>
2024-10-19 16:06:49 +0530

    Time: 1 ms (77.74%), Space: 41 MB (90.21%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        Queue<Integer> iter = new LinkedList<Integer>();
+        Queue<String> q = new LinkedList<String>();
+
+        for(String s : bank)
+        {
+            map.put(s,0);
+        }
+
+        q.offer(startGene);
+        iter.offer(0);
+        
+        while( q.peek() != null )
+        {
+            String gene = q.poll();
+            int count = iter.poll();
+
+            if( endGene.equals(gene))
+                return count;
+
+            for(String b : bank)
+            {
+                if( mutates(gene, b) && map.get(b) == 0)
+                {
+                    q.offer(b);
+                    iter.offer(count + 1);
+                    map.put(b,1);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static boolean mutates(String a, String b)
+    {
+        int c = 0;
+        for(int i=0;i<8;i++)
+        {
+            if( a.charAt(i) != b.charAt(i) )
+                c += 1;
+        }
+        return c == 1;
+    }
+}
